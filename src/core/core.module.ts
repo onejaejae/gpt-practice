e83be11676831459ccb117f1725cbc10f75de6db
wonjjee <yoteamo7@naver.com>
2025-01-18 13:40:47 +0900

    feat: added core module

---
+++ src/core/core.module.ts
@@ -0,0 +1,24 @@
+import { ClassProvider, Global, Module } from '@nestjs/common';
+import { ConfigModule } from './config/config.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ApiResponseInterceptor } from './interceptor/apiResponse.interceptor';
+import { ErrorInterceptor } from './interceptor/error.interceptor';
+import { JWTModule } from './jwt/jwt.module';
+import { TypeOrmModule } from './database/typeorm/typeorm.module';
+import { GptModule } from './gpt/gpt.module';
+
+const modules = [ConfigModule, JWTModule, GptModule];
+const providers = [];
+const interceptors: ClassProvider[] = [
+  { provide: APP_INTERCEPTOR, useClass: ApiResponseInterceptor },
+  { provide: APP_INTERCEPTOR, useClass: ErrorInterceptor },
+];
+const filters: ClassProvider[] = [];
+
+@Global()
+@Module({
+  imports: [TypeOrmModule.forRoot(), ...modules],
+  providers: [...providers, ...interceptors, ...filters],
+  exports: [...modules, ...providers],
+})
+export class CoreModule {}
