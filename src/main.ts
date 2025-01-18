1ece4af0db2996e06fa9cc703d52456e65c02602
wonjjee <yoteamo7@naver.com>
2025-01-18 13:34:20 +0900

    feat: setup nest

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { setNestApp } from 'setNestApp';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  setNestApp(app);
+
+  await app.listen(3000);
+}
+bootstrap();
