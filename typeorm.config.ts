b4d1b32eaa522869d6a8feeb2f5abb5d48bec686
wonjjee <yoteamo7@naver.com>
2025-01-18 13:43:36 +0900

    feat: added typeorm.config.ts for seed 및 entity migration

---
+++ typeorm.config.ts
@@ -0,0 +1,39 @@
+import { config } from 'dotenv';
+import { ChatHistory } from 'src/entities/chat-history/chat-history.entity';
+import { User } from 'src/entities/user/user.entity';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SeederOptions } from 'typeorm-extension';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+const configDataSource = async () => {
+  console.log('NODE_ENV', process.env.NODE_ENV);
+  const nodeEnv = process.env.NODE_ENV ?? 'local';
+  config({ path: `./dotenv/.env.${nodeEnv}` });
+
+  const options: DataSourceOptions & SeederOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '', 10),
+    username: process.env.DB_USER_NAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [User, ChatHistory],
+    namingStrategy: new SnakeNamingStrategy(),
+    synchronize: false,
+    logging: true,
+    migrations: [__dirname + '/src/core/database/typeorm/migrations/*.ts'],
+    migrationsRun: false,
+    migrationsTableName: 'migrations',
+    migrationsTransactionMode: 'each',
+    seeds: [__dirname + '/src/core/database/typeorm/seeds/*.seeder.ts'],
+    seedTracking: false,
+    factories: [
+      __dirname + '/src/core/database/typeorm/seeds/factories/**/*.ts',
+    ],
+  };
+  console.log(options);
+
+  return options;
+};
+
+export default configDataSource().then((opt) => new DataSource(opt));
