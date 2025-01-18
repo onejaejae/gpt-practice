3e39a0247c76b1cdeb2c59eada9bb42a10b471e0
wonjjee <yoteamo7@naver.com>
2025-01-18 13:35:22 +0900

    feat: added typeorm module

---
+++ src/core/database/typeorm/typeorm.interface.ts
@@ -0,0 +1,36 @@
+import { Union } from 'src/common/type/common.interface';
+import { FindOptionsRelations } from 'typeorm';
+
+export type OmitUppercaseProps<T> = {
+  [K in keyof T as K extends string
+    ? K extends `${Uppercase<string>}${string}`
+      ? never
+      : K
+    : K]: T[K];
+};
+
+export type OmitNotJoinedProps<T, R extends FindOptionsRelations<T>> = {
+  [K in keyof T as K extends string
+    ? K extends `${Uppercase<string>}${string}`
+      ? K extends keyof R
+        ? K
+        : never
+      : K
+    : K]: K extends keyof R
+    ? R[K] extends true
+      ? T[K] extends (infer U)[]
+        ? OmitUppercaseProps<U>[]
+        : OmitUppercaseProps<T[K]>
+      : R[K] extends object
+        ? T[K] extends (infer U)[]
+          ? OmitNotJoinedProps<U, R[K]>[]
+          : OmitNotJoinedProps<T[K], R[K]>
+        : T[K]
+    : T[K];
+};
+
+export const LockModeType = {
+  PessimisticWrite: 'pessimistic_write',
+  PessimisticRead: 'pessimistic_read',
+} as const;
+export type LockModeType = Union<typeof LockModeType>;
